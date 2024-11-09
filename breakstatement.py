7f928e5cc645f2f067078d5994922736b7320198
pnparthivkumar13201105 <pnparthivkumar13@gmail.com>
2024-11-09 15:49:30 +0530

    keywords

---
+++ breakstatement.py
@@ -0,0 +1,7 @@
+str=input("enter any statement:--")
+for i in str:
+    if i=='a':
+        print("letter a has ouccured in this statement")
+        break
+    else:
+        print("letter a has not ouccered in this statement")
